5ad31f97344f507874ddeddfa34e89d675b02099
Chase Goesling <chase.goesling@gmail.com>
2020-12-01 09:49:28 -0800

    Adds html to path '/movies/'. Modifies URL parameters that allow for pages to be linked together. Raises 404 errors instead of DoesNotExist errors.

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views # Note, due to how django loads modules, import views, will not work
 
+app_name = 'movies' #new
+
 # urlpattern is 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),                #modified
+    path('<int:movie_id>', views.detail, name='detail') #modified
 ]
